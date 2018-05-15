63781fca8a7ebe585d0b21aaff704d36b95abddf
sabatte lilian <l.sabatte@jbrel.fr>
2018-05-15 12:02:19 +0200

    avancement page contact

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,11 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use Redirect;
 use Request;
+use Validator;
 
 class ContactController extends Controller
 {
     public function index() {
         return view('contact.index');
+    }
+
+    public function store() {
+        //dd($_POST);
+        $values = Request::all();
+
+        $rules =
+        [
+            'email' => 'email|required',
+            'name' => 'string',
+            'lastname' => 'string',
+            'object' => 'string',
+            'message' => 'string',
+        ];
+
+        $validator = Validator::make($values, $rules,
+        [
+           'email.email' => 'Votre email est invalid',
+           'name.string' => 'Votre prénom est obligatoire',
+           'lastname.string' => 'Votre nom de famille est obligatoire',
+           'object.string' => 'Votre object est obligatoire',
+           'message.string' => 'Votre message est obligatoire',
+        ]);
+
+        if($validator->fails()) {
+            return Redirect::back()
+                            ->withErrors($validator)
+                            ->withInput();
+        }
+
     }
 }
