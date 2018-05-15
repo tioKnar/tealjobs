f8a621fb7e271060b151e8e54b0c54958b80f464
Philippe <mrphilippe.dev@gmail.com>
2018-05-15 11:15:03 +0200

    package.json

--- resources/views/landingpage/index.blade.php
+++ resources/views/landingpage/index.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-	<div class="container-fluid">
+	<div class="container-fluid jumbo">
 		<div class="row">
 			<div class="col- col-lg-12">
 				<div class="jumbotron jumbotron-fluid mx-auto">
-				  <div class="container-fluid">
+				  <div class="container-fluid jumbo-text">
 					<p class="lead">Il n'y a pas d'âge pour apprendre ...à se connaître.</p>
 				  	<hr class="my-4">
 				  	<p>Orientation post bac, reconversion professionnelle ou indécisions de toujours, tealJobs vous oriente vers les métiers d'avenir qui correspondent le mieux à votre personnalité.</p>
-				  	<p class="lead">
+				  	<p class="lead align-items-center">
 				    	<a class="btn btn-dark btn-lg" href="/test" role="button">Testez-vous !</a>
 				  	</p>
 				  </div>
