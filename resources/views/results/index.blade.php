b2e9236d4ea6a87e87cdf95a89bfa4b2845aa195
Philippe M <mrphilippe.dev@gmail.com>
2018-05-16 17:10:39 +0200

    17:10

--- resources/views/results/index.blade.php
+++ resources/views/results/index.blade.php
@@ -5,7 +5,12 @@
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-12">
-				<img src="images/result_page.jpg" id="result_tree" alt="result_tree">
+				<img src="images/result_page_pano.jpg" class="img-fluid" id="result-tree" alt="result_tree">
+
+				@foreach()
+
+				@endforeach
+
 				<div class="fireflies">@include('fireflies.index')</div>
 			</div>
 		</div>
