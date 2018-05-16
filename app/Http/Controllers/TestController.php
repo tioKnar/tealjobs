d7a18897446463c18a5684bfb97cae43ca79723a
donatientaffard <donatientaffard@gmail.com>
2018-05-16 09:15:52 +0200

    wesh

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -12,20 +12,54 @@ class TestController extends Controller
 {
     public function index(){
 
-    	$allQuestions = Question::all();
-    	$totalQuestions = $allQuestions->count();
+    	// $allQuestions = Question::all();
+    	// $totalQuestions = $allQuestions->count();
 
-    	$item = 1;
+    	// $item = 1;
 
-    	$question = Question::find($item);
+    	$question1 = Question::find(1);
     	
-    	$reponses = Answer::all()->where('id', '=', $item);
+    	$reponses1 = Answer::all()->where('id', '=', 1);
+
+    	$question2 = Question::find(2);
+
+    	$reponses2 = Answer::all()->where('id', '=', 2);
+
+    	$question3 = Question::find(3);
+    	
+    	$reponses3 = Answer::all()->where('id', '=', 3);
+
+    	$question4 = Question::find(4);
+
+    	$reponses4 = Answer::all()->where('id', '=', 4);
+
+    	$question5 = Question::find(5);
+    	
+    	$reponses5 = Answer::all()->where('id', '=', 5);
+
+    	$question6 = Question::find(6);
+
+    	$reponses6 = Answer::all()->where('id', '=', 6);
 
     	// dd($question);
 
-    	return view('test.index')->with('question', $question)
-    							 ->with('reponses', $reponses)
-    							 ->with('item', $item)
-    							 ->with('totalQuestions', $totalQuestions);
+    	// $questions = Question::all();
+
+    	// $reponses = Answer::all();
+
+    	return view('test.index')->with('question1', $question1)
+    							 ->with('question2', $question2)
+    							 ->with('question3', $question3)
+    							 ->with('question4', $question4)
+    							 ->with('question5', $question5)
+    							 ->with('question6', $question6)
+    							 ->with('reponses1', $reponses1)
+    							 ->with('reponses2', $reponses2)
+    							 ->with('reponses3', $reponses3)
+    							 ->with('reponses4', $reponses4)
+    							 ->with('reponses5', $reponses5)
+    							 ->with('reponses6', $reponses6);
+    							 // ->with('item', $item)
+    							 // ->with('totalQuestions', $totalQuestions);
     }
 }
--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -6,9 +6,10 @@
 			<div class="row">
 				<div class="col">
 					<div id="content">
-							@if(! empty($question))
-							{{ $question->question }} <br>
-								@foreach($reponses as $reponse)
+						<div id="1">
+							@if(! empty($question1))
+							{{ $question1->question }} <br>
+								@foreach($reponses1 as $reponse)
 									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer1 }}</label><br>
 									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer2 }}</label><br>
 									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer3 }}</label><br>
@@ -17,8 +18,73 @@
 									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer6 }}</label><br>
 								@endforeach
 							@endif
-				
-							<button id="back" disabled>Back</button>
+						</div>
+						<div id="2" hidden="true">
+							@if(! empty($question2))
+							{{ $question2->question }} <br>
+								@foreach($reponses2 as $reponse)
+									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer1 }}</label><br>
+									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer2 }}</label><br>
+									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer3 }}</label><br>
+									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer4 }}</label><br>
+									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer5 }}</label><br>
+									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer6 }}</label><br>
+								@endforeach
+							@endif
+						</div>
+						<div id="2" hidden="true">
+							@if(! empty($question3))
+							{{ $question2->question }} <br>
+								@foreach($reponses3 as $reponse)
+									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer1 }}</label><br>
+									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer2 }}</label><br>
+									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer3 }}</label><br>
+									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer4 }}</label><br>
+									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer5 }}</label><br>
+									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer6 }}</label><br>
+								@endforeach
+							@endif
+						</div>
+						<div id="2" hidden="true">
+							@if(! empty($question4))
+							{{ $question2->question }} <br>
+								@foreach($reponses4 as $reponse)
+									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer1 }}</label><br>
+									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer2 }}</label><br>
+									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer3 }}</label><br>
+									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer4 }}</label><br>
+									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer5 }}</label><br>
+									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer6 }}</label><br>
+								@endforeach
+							@endif
+						</div>
+						<div id="2" hidden="true">
+							@if(! empty($question5))
+							{{ $question2->question }} <br>
+								@foreach($reponses5 as $reponse)
+									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer1 }}</label><br>
+									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer2 }}</label><br>
+									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer3 }}</label><br>
+									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer4 }}</label><br>
+									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer5 }}</label><br>
+									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer6 }}</label><br>
+								@endforeach
+							@endif
+						</div>
+						<div id="2" hidden="true">
+							@if(! empty($question6))
+							{{ $question2->question }} <br>
+								@foreach($reponses6 as $reponse)
+									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer1 }}</label><br>
+									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer2 }}</label><br>
+									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer3 }}</label><br>
+									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer4 }}</label><br>
+									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer5 }}</label><br>
+									<input type="checkbox" id="{{ $reponse->id }}"><label for="{{ $reponse->id }}">{{ $reponse->answer6 }}</label><br>
+								@endforeach
+							@endif
+						</div>
+							<button id="back" disabled="true">Back</button>
 							<button id="next">Next</button>
 			        </div> 
 				</div>
@@ -28,15 +94,11 @@
 			$(function(){
 				$('#next').on('click', function(){
 					var profile = [];
-					var item = {{ $item }};
-					var totalQuestions = {{ $totalQuestions }}; 
-					$('#content').fadeOut(500, function(){
-						if(item < totalQuestions){
-							
-						}
+					$('#1').fadeOut(500, function(){
+						$('#back').attr('disabled', false);						
+					}).replaceWith();
+					$('#2').attr('hidden', false).fadeIn(500);
 
-					});
-					$('#content').fadeIn(500);
 				});
 			});
 		</script>
