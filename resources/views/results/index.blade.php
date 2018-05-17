74a32188c61f9f321905394257472a1a11996b54
Philippe M <mrphilippe.dev@gmail.com>
2018-05-17 13:58:59 +0200

    yo yo

--- resources/views/results/index.blade.php
+++ resources/views/results/index.blade.php
@@ -6,14 +6,36 @@
 		<div class="row">
 			<div class="col-12">
 				<img src="images/result_page_pano.jpg" class="img-fluid" id="result-tree" alt="result_tree">
+					<div id="foliage-square">	
+						<div id="foliage"></div>
+					</div>
 
-				@foreach()
-
-				@endforeach
-
-				<div class="fireflies">@include('fireflies.index')</div>
+				{{-- <div class="fireflies">@include('fireflies.index')</div> --}}
 			</div>
 		</div>
 	</div>
+
+{{-- <script>
+	
+	$(function() {
+
+		let $width = $('#foliage').width();
+		let $height = $('#foliage').height();
+			
+		@foreach($results as $result) {
+			 var $left = Math.floor(Math.random() * (width-100));
+			 var $top = Math.floor(Math.random() * (height-100));
+
+			 $("<div id='fruit'>{{$result->name}}</div>").appendTo("#foliage").css({
+			 	left: $left;
+			 	top: $top;
+			 })
+		}
+
+		@endforeach
+
+	};
+	
+</script> --}}
 
 @endsection
