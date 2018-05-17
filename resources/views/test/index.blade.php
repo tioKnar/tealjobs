eecc0ec376157418c4d38fa76672453a25d34225
donatientaffard <donatientaffard@gmail.com>
2018-05-17 11:22:28 +0200

    blabla

--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -6,80 +6,29 @@
 	<div class="container-fluid" >
 
 		<div class="row justify-content-center text-center" id="test">
-
-			<div class="col-md-4" id="id1">
 				
-				<br><h3>Question 1</h3><br>
+				@foreach($resultats as $resultat)
+					<div class="col-md-4 test @if($resultat->question_id != 1) {{ 'completed' }} @endif">
+						<br><h3>{{ $resultat->question }}</h3>
+								<button type="button" class="form-control form-group btn btn-info bouton" data-id="1">{{ $resultat->answer1 }}</button>
+								<button type="button" class="form-control form-group btn btn-info bouton" data-id="2">{{ $resultat->answer2 }}</button>
+								<button type="button" class="form-control form-group btn btn-info bouton" data-id="3">{{ $resultat->answer3 }}</button>
+								<button type="button" class="form-control form-group btn btn-info bouton" data-id="4">{{ $resultat->answer4 }}</button>
+								<button type="button" class="form-control form-group btn btn-info bouton" data-id="5">{{ $resultat->answer5 }}</button>
+								<button type="button" class="form-control form-group btn btn-info bouton" data-id="6">{{ $resultat->answer6 }}</button>
 
-				<button type="button" class="form-control form-group btn btn-info bouton" data-id=1>Réponse 1</button>
-				<button type="button" class="form-control form-group btn btn-info bouton" data-id=2>Réponse 2</button>
-				<button type="button" class="form-control form-group btn btn-info bouton" data-id=3>Réponse 3</button>
-				<button type="button" class="form-control form-group btn btn-info bouton" data-id=4>Réponse 4</button>
-				<button type="button" class="form-control form-group btn btn-info bouton" data-id=5>Réponse 5</button>
-				<button type="button" class="form-control form-group btn btn-info bouton" data-id=6>Réponse 6</button>
-
-			</div>
-
-			<div class="col-md-4 completed" id="id2">
-	
-				<br><h3>Question 2</h3><br>
-
-				<button type="button" class="form-control form-group btn btn-info boutn" data-id=1>Réponse 1</button>
-				<button type="button" class="form-control form-group btn btn-info bouton" data-id=2>Réponse 2</button>
-				<button type="button" class="form-control form-group btn btn-info bouton" data-id=3>Réponse 3</button>
-				<button type="button" class="form-control form-group btn btn-info bouton" data-id=4>Réponse 4</button>
-				<button type="button" class="form-control form-group btn btn-info bouton" data-id=5>Réponse 5</button>
-				<button type="button" class="form-control form-group btn btn-info bouton" data-id=6>Réponse 6</button>
-
-			</div>
-
-			<div class="col-md-4 completed" id="id3">
-				
-				<br><h3>Question 3</h3><br>
-
-				<button type="button" class="form-control form-group btn btn-info bouton" data-id=1>Réponse 1</button>
-				<button type="button" class="form-control form-group btn btn-info bouton" data-id=2>Réponse 2</button>
-				<button type="button" class="form-control form-group btn btn-info bouton" data-id=3>Réponse 3</button>
-				<button type="button" class="form-control form-group btn btn-info bouton" data-id=4>Réponse 4</button>
-				<button type="button" class="form-control form-group btn btn-info bouton" data-id=5>Réponse 5</button>
-				<button type="button" class="form-control form-group btn btn-info bouton" data-id=6>Réponse 6</button>
-
-			</div>
-
-			<div class="col-md-4 completed" id="id4">
-				
-				<br><h3>Question 4</h3><br>
-
-				<button type="button" class="form-control form-group btn btn-info bouton" data-id=1>Réponse 1</button>
-				<button type="button" class="form-control form-group btn btn-info bouton" data-id=2>Réponse 2</button>
-				<button type="button" class="form-control form-group btn btn-info bouton" data-id=3>Réponse 3</button>
-				<button type="button" class="form-control form-group btn btn-info bouton" data-id=4>Réponse 4</button>
-				<button type="button" class="form-control form-group btn btn-info bouton" data-id=5>Réponse 5</button>
-				<button type="button" class="form-control form-group btn btn-info bouton" data-id=6>Réponse 6</button>
-
-			</div>
-
-			<div class="col-md-4 completed" id="id5">
-				
-				<br><h3>Question 5</h3><br>
-
-				<button type="button" class="form-control form-group btn btn-info bouton" data-id=1>Réponse 1</button>
-				<button type="button" class="form-control form-group btn btn-info bouton" data-id=2>Réponse 2</button>
-				<button type="button" class="form-control form-group btn btn-info bouton" data-id=3>Réponse 3</button>
-				<button type="button" class="form-control form-group btn btn-info bouton" data-id=4>Réponse 4</button>
-				<button type="button" class="form-control form-group btn btn-info bouton" data-id=5>Réponse 5</button>
-				<button type="button" class="form-control form-group btn btn-info bouton" data-id=6>Réponse 6</button>
-
-			</div>
-
+								
+								<button class="previous">previous</button>
+								<!-- <button class="next">next</button> -->
+					</div>
+				@endforeach
 		</div>
 
 	</div>
 
 <br><br>
 
-<button id="next">next</button>
-<button id="previous">previous</button>
+
 
 <script>
 	
@@ -98,9 +47,23 @@ $(function() {
 		console.log($tab);	
 	})
 
-	$('.next').on('click', function() {
+	// $('.next').on('click', function() {
 
+	// 	$(this).parent().addClass('completed');
+			
+	// 	$(this).parent().next().removeClass('completed');
 		
+	// })
+
+	$('.previous').on('click', function() {
+
+		$(this).parent().addClass('completed');
+			
+		$(this).parent().prev().removeClass('completed');
+
+		$tab.pop();
+		
+		console.log($tab);
 	})
 });
 
