ba3d9a4c44ecd0b2b77965d1d90eb6384a082e59
Magalus <37544780+Magalus@users.noreply.github.com>
2018-05-18 17:40:01 +0200

    ON VOIT LE BOUT DU TUNNEL

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -103,27 +103,20 @@
 
             <div class="footer-right">
 
-                <a href="#"><i class="fa fa-facebook"></i></a>
-                <a href="#"><i class="fa fa-twitter"></i></a>
-                <a href="#"><i class="fa fa-linkedin"></i></a>
-                <a href="#"><i class="fa fa-github"></i></a>
+                <a href="https://www.facebook.com"><i class="fa fa-facebook"></i></a>
+                <a href="https://www.twitter.com"><i class="fa fa-twitter"></i></a>
+                <a href="https://www.linkedin.com"><i class="fa fa-linkedin"></i></a>
+                <a href="https://www.github.com"><i class="fa fa-github"></i></a>
 
             </div>
 
             <div class="footer-left">
 
                 <p class="footer-links">
-                    <a href="#">Home</a>
+
+                    <a href="contact">Contact</a>
                     ·
-                    <a href="#">Blog</a>
-                    ·
-                    <a href="#">Pricing</a>
-                    ·
-                    <a href="#">About</a>
-                    ·
-                    <a href="#">Faq</a>
-                    ·
-                    <a href="#">Contact</a>
+                    <a href="#">Mentions légales</a>
                 </p>
 
                 <p>Company TealJobs &copy; 2018</p>
