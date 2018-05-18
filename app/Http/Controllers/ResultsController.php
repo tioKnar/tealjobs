6ecfb6b3a0520a60135fa3e75261b5e22c7a8a0d
Philippe <mrphilippe.dev@gmail.com>
2018-05-18 14:41:19 +0200

    on avance...

--- app/Http/Controllers/ResultsController.php
+++ app/Http/Controllers/ResultsController.php
@@ -2,24 +2,30 @@
 
 namespace App\Http\Controllers;
 
-// use Request;
-// use App\Model\Answer;
-// use App\Model\Analyse;
-// use Redirect;
-// use DB;
+use Request;
+use App\Model\Analyse;
+use App\Model\Answer;
+use App\Model\Intersector;
+use App\Model\Job;
+use Redirect;
+use DB;
 
 
 class ResultsController extends Controller
 {
     public function index(){
 
-        // $resultats = DB::table('questions')
-        //                 ->join('interanswers', 'questions.id', '=', 'interanswers.question_id')
-        //                 ->join('answers', 'interanswers.answer_id', '=', 'answers.id')
-        //                 ->get();
+         $jobs = DB::table('jobs')
+                         ->join('intersectors', 'jobs.id', '=', 'intersectors.job_id')
+                         ->join('analyses', 'intersectors.sector_id', '=', 'analyses.sector_id')
+                         ->where('analyses.resultprofile' , '=',  '43')
+                         ->get();
+        
+         $randomize = $jobs->random(3)
+                           ->values();
 
-        return view('results.index');
-        //      ->export();>with('resultats', $resultats);
+        return view('results.index')
+              ->with('results', $randomize);
     }
 
     
--- resources/views/results/index.blade.php
+++ resources/views/results/index.blade.php
@@ -2,30 +2,13 @@
 
 @section('content')
 
-
-<?php
-
-	$tests = [
-		[  
-			'nom' =>  'Romain',
-			'job' =>  'Drag Queen'							
-		],
-		[
-			'nom' => 'Caroline',
-			'job' => 'Pompier'
-		]
-	];
-// dd($tests);
-?>
-
-
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-12">
 				<div id="result-bg" alt="result_tree">
 					<div id="foliage-square">
-						@foreach($tests as $test)
-							<div class="job" >{{$test['job']}}</div>
+						@foreach($results as $result)
+							<div class="job">{{'result.name'}}</div>
 							<div class="fruit"></div>
 						@endforeach
 					</div>				
@@ -35,28 +18,5 @@
 			</div>
 		</div>
 	</div>
-
-{{-- <script>
-	
-	$(function() {
-
-		let $width = $('#foliage').width();
-		let $height = $('#foliage').height();
-			
-		@foreach($tests as $test) {
-			 var $left = Math.floor(Math.random() * (width-100));
-			 var $top = Math.floor(Math.random() * (height-100));
-
-			 $("<div class='job'>{{$test['job']}}</div> <img class='fruit' src='images/fruit.png'>").appendTo("#foliage-square").css({
-			 	left: $left;
-			 	top: $top;
-			 })
-		}
-
-		@endforeach
-
-	};
-	
-</script> --}}
 
 @endsection
