8227ca2b9afee92111bb57dabeeb1b2d526e6411
sabatte lilian <l.sabatte@jbrel.fr>
2018-05-24 12:12:04 +0200

    partners

--- resources/views/partners/index.blade.php
+++ resources/views/partners/index.blade.php
@@ -2,4 +2,55 @@
 
 @section('content')
 
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <br><h1 class="title-partners d-flex justify-content-center">Nos Principaux Partenaires</h1>
+                <hr class="hr-partners"><br>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-4">
+                <a href="https://www.greta-aquitaine.fr/"><img class="logo-partners" src="images/logo.png" alt="logo-partenaires"></a>
+            </div>
+            <div class="col-md-4">
+                <a href="http://bordeauxgironde.cci.fr/"><img class="logo-partners" src="images/logo (1).png" alt="logo-partenaires"></a>
+            </div>
+            <div class="col-md-4">
+                <a href="https://www.cafa-formations.com/"><img class="logo-partners" src="images/logo-cafa-bordeaux.png" alt="logo-partenaires"></a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <a href="https://www.cesi-entreprises-event.fr/site/home.html"><img class="logo-partners" src="images/logo-cesi-entreprises.png" alt="logo-partenaires"></a>
+            </div>
+            <div class="col-md-4">
+                <a href="http://www.afec.fr/centres-de-formation-afec/afec-bordeaux-centre-de-formation"><img class="logo-partners" src="images/logo_1.png" alt="logo-partenaires"></a>
+            </div>
+            <div class="col-md-4">
+                <a href="http://trends-academy.fr/"><img class="logo-partners" src="images/logo_trenadsacademy-2017.png" alt="logo-partenaires"></a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <a href="http://bordeauxformation.com/"><img class="logo-partners" src="images/logoACF.png" alt="logo-partenaires"></a>
+            </div>
+            <div class="col-md-4">
+                <a href="http://www.philomathiquebordeaux.com/"><img class="logo-partners" src="images/LogoPhilomathiqueVertH140.png" alt="logo-partenaires"></a>
+            </div>
+            <div class="col-md-4">
+                <a href="http://www.orsys.fr/"><img class="logo-partners" src="images/orsys.png" alt="logo-partenaires"></a>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
 @endsection
