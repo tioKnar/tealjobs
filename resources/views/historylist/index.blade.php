e3ae29c6727418ec77098497ead8d0a433d7c836
Magalus <37544780+Magalus@users.noreply.github.com>
2018-05-25 11:00:27 +0200

    un des derniers commit :'(

--- resources/views/historylist/index.blade.php
+++ resources/views/historylist/index.blade.php
@@ -20,53 +20,36 @@
 
 	<div class="row justify-content-center">
 
+		<?php $e=1 ?>
 		@foreach($jobs as $job)
-				
-			<div class="card-deck col-md-4">
-					
-				<div class="card border-primary" style="max-width: 18rem;">
-					
-					<div class="card-header">{{$job->name}}</div>
+			
+			<div class="card-deck col-md-4" data-toggle="modal" data-target="#modaljob{{$e}}">
 						
-						<div class="card-body text-primary">
-							 
-							 <h5 class="card-title"></h5>
-							 <p class="card-text">{{$job->description}}</p>
-						</div>
+				<div class="card border-primary" style="max-width: 18rem;">
+						
+					<div class="card-header">
+
+						{{$job->name}}
+
+					</div>
+							
+					<div class="card-body text-primary">
+								 
+						<h5 class="card-title"></h5>
+						<p class="card-text">{{$job->description}}</p>
 
 					</div>
 
+				</div>
+
 			</div>
 		
+		@include('modal.job')
+		<?php $e++ ?>
 		@endforeach
 
 	</div>
 
 </div>
-
-		{{-- @foreach($classes as $classe)
-
-			@if($classe->jobs_id == $job->job_id)
-
-				<div class="card card-body">
-
-					<div>
-						 
-						<h4>{{ $classe->classes_name }}</h4><hr>
-						<strong>Durée :</strong> {{ $classe->duration }} mois<br>
-						<strong>Coût :</strong> {{ $classe->cost }} €<br>
-						<strong>Contact :</strong> {{ $classe->contact }}<br>
-						<strong>Adresse :</strong> {{ $classe->address }}, {{ $classe->cp }}, {{ $classe->city }}<br>
-						<strong>Email :</strong> {{ $classe->mail }}<br>
-						<strong>Téléphone :</strong> {{ $classe->tel }}<br>
-						<strong>Site :</strong><a href="{{ $classe->link }}"> {{ $classe->link }} </a>
-
-					</div>
-
-					</div>
-
-			@endif
-
-		@endforeach --}}
 
 @endsection
